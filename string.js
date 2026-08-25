054649adcd26b8073b823d39df4c5375df1509f5
Abdullah Baki <abz.147570@gmail.com>
2026-08-25 21:04:43 +0600

    random-string

--- string.js
+++ string.js
@@ -3,3 +3,7 @@ let lastName = "Doe";
 // let fullName = firstName + " " + lastName;
 let fullName = `${firstName} ${lastName} \nNaogaon`; // Using template literals for string interpolation
 console.log(fullName); // Output: John Doe
+
+// let randomstr = 'This is a random "string"';
+let randomstr = "This is a random \"string\"";
+console.log(randomstr.length); // Output: 25
