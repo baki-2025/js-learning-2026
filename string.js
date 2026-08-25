8ed93ad8d3546618b2b8ebf7aa23ca111c9bbc4c
Abdullah Baki <abz.147570@gmail.com>
2026-08-25 20:54:04 +0600

    string

---
+++ string.js
@@ -0,0 +1,5 @@
+let firstName = "John";
+let lastName = "Doe";
+// let fullName = firstName + " " + lastName;
+let fullName = `${firstName} ${lastName} \nNaogaon`; // Using template literals for string interpolation
+console.log(fullName); // Output: John Doe
