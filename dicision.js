a712da1a1f19fba28367f1ae58202dcad36a9a6d
Abdullah Baki <abz.147570@gmail.com>
2026-08-26 21:36:34 +0600

    marks

--- dicision.js
+++ dicision.js
@@ -5,3 +5,12 @@ if (age >= 18){
 }else{
     console.log("You are not eligible to vote");
 }
+
+let marks = 80
+if (marks >= 80 || marks >=90){
+    console.log("You got an A+ grade");
+}else if (marks >= 60){
+    console.log("You got a B grade");
+}else{
+    console.log("You got a C grade");
+}
