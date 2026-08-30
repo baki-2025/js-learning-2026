2769e04547ba34600436a9b92af0cad674a75512
Abdullah Baki <abz.147570@gmail.com>
2026-08-30 23:17:49 +0600

    spend

--- function.js
+++ function.js
@@ -1,5 +1,12 @@
-function total(a, b) {
-    return a + b;
-}
+// function total(a, b) {
+//     return a + b;
+// }
 
-console.log(total(5, 10));
+// console.log(total(5, 10));
+
+function total(a, b) {
+    const grandTotal = a + b;
+    return grandTotal;
+}
+ const spend = total(5, 10);
+ console.log(spend);
