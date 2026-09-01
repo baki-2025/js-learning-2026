c320b2c847950860b2758794f0d5aeff2677c608
Abdullah Baki <abz.147570@gmail.com>
2026-09-01 08:32:53 +0600

    array/push,pop,unshift,shift,splice

---
+++ array2.js
@@ -0,0 +1,10 @@
+let name = ["Aslam", "Ali", "Khan"];
+let number = [10, 20, 30, 40, 50];
+// console.log(name[0], number[2]);
+
+name.push("Ahmed", "Hassan");
+name.pop();
+name.shift();
+name.unshift("Zain");
+name.splice(2,0, "Usman");
+console.log(name);
