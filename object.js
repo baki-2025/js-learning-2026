deda2bbfe404e855d56016071ec4116c0a1d3c80
Abdullah Baki <abz.147570@gmail.com>
2026-09-01 13:10:26 +0600

    entries/object

---
+++ object.js
@@ -0,0 +1,20 @@
+let user = {
+    name: "Aslam",
+    age: 25,
+    address: {
+        street: "123 Main St",
+        city: "Anytown",
+        state: "CA",
+        zip: "12345"
+    },
+    email: "aslam@example.com"
+}
+// console.log(user["name"]); //user[`${}`]
+delete user.address;
+user.address = {
+    street: "456 Oak Ave",
+    city: "Rawalpindi",
+    state: "Punjab",
+    zip: "40000"
+};
+console.log(Object.entries(user));
