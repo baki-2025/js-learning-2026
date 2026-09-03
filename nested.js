a5cb0c569be9830cadacca8ba04327588fb60e82
Abdullah Baki <abz.147570@gmail.com>
2026-09-03 22:29:32 +0600

    nested-array

---
+++ nested.js
@@ -0,0 +1,14 @@
+let user = {
+    name: "Aslam",
+    age: 25,
+    address: {
+        street: "123 Main St",
+        city: "Anytown",
+        state: "CA",
+        zip: "12345"
+    },
+    email: "aslam@example.com"
+};
+
+console.log(user["address"]["zip"]); 
+
