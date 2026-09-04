95769a1fb3c802f5cc317ca6b5a31662496a1b6d
Abdullah Baki <abz.147570@gmail.com>
2026-09-04 09:05:03 +0600

    loop-for/while

---
+++ loop.js
@@ -0,0 +1,24 @@
+// for and while loop****
+
+
+// for (let i = 0; i < 5; i++) {
+//     // console.log(i);
+//     console.log(`The value of i is: ${i}`);
+// }
+
+// for (let i = 4; i >= 0; i--) {
+//     // console.log(i);
+//     console.log(`count: ${i}`);
+// }
+
+let fruits = ["apple", "banana", "cherry", "date", "elderberry"];
+
+// for (let i = 0; i < fruits.length; i++) {
+//     console.log(`khabo: ${fruits[i]}`);
+// }
+
+let i = 0;
+while (i < fruits.length) {
+    console.log(`khabo: ${fruits[i]}`);
+    i++;
+}
