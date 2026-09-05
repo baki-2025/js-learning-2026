4fc4d20c81df2d61f72d3039b908024a80c9c6bd
Abdullah Baki <abz.147570@gmail.com>
2026-09-05 20:07:35 +0600

    array-ops/updates

--- array-ops.js
+++ array-ops.js
@@ -1,10 +1,23 @@
 let marks = [50, 60, 70, 80, 90, 100];
 
-let max = marks[0];
+// let max = marks[0];
 
-for (let i = 1; i < marks.length; i++) {
-    if (marks[i] > max) {
-        max = marks[i];
-    }
+// for (let i = 1; i < marks.length; i++) {
+//     if (marks[i] > max) {
+//         max = marks[i];
+//     }
+// }
+// console.log(max);
+
+// let sum = 0;
+// for (let mark of marks) {
+//     sum += mark;
+// }
+// console.log(sum);
+// console.log(sum / marks.length);
+
+let reMarks = [];
+for (let i = marks.length - 1; i >= 0; i--) {
+    reMarks.push(marks[i]);
 }
-console.log(max);
+console.log(reMarks);
