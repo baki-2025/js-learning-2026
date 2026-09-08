4ba0c476e1ee96318431a8eb88f942b11b7f4ab5
Abdullah Baki <abz.147570@gmail.com>
2026-09-09 00:48:24 +0600

    discount

--- literals.js
+++ literals.js
@@ -1,4 +1,10 @@
 let price = 500;
 let quantity = 3;
 
-console.log(`Total price: ${price*quantity}Taka\n pay to price to get it as your own`,);
+// console.log(`Total price: ${price*quantity}Taka\n pay to price to get it as your own`,);
+
+function getDiscount(price){
+    return price*0.1;
+}
+
+console.log(`You saved: ${getDiscount(price)} taka\n use this as an investment`);
