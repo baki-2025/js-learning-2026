be3216c89e8e443f722e0868af46486eb1c99936
Abdullah Baki <abz.147570@gmail.com>
2026-09-09 13:32:43 +0600

    hobbies

--- destructuring.js
+++ destructuring.js
@@ -1,11 +1,49 @@
-const student = {
-    name: "Rahim" ,
+// const student = {
+//     name: "Rahim" ,
+//     age: 20,
+//     address: "Dhaka"
+// }
+
+// const OldName = student.name
+
+// const {age, address, name} = student
+
+// console.log(name);
+
+// const student1 = {
+//     name: "Rahim" ,
+//     age: 20,
+//     address: {
+//         city: "Dhaka",
+//         zip: 131,
+//     },
+   
+// }
+
+// const {name: stdName,
+//     address:{city, zip},
+// } = student1;
+
+// console.log(stdName,city);
+
+const arr = ["Red", "Green", "Blue"];
+const [first, second, third] = arr;
+
+// console.log(second);
+
+const student2 = {
+    name: "Karim" ,
     age: 20,
-    address: "Dhaka"
+    address: {
+        city: "Raj",
+        zip: 131,
+    },
+    hobbies: ["Gardenning", "Travelling", "Cooking"],
 }
 
-const OldName = student.name
-
-const {age, address, name} = student
-
-console.log(name)
+const {
+    name: std2Name ,
+    address: { city: stdCity },
+    hobbies: {firstHobby}
+} = student2;
+console.log(firstHobby, stdCity);
