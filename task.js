f2b80892d34f47676e859cc3df9a23fc21bdc3a3
Abdullah Baki <abz.147570@gmail.com>
2026-09-14 21:34:57 +0600

    movie/task

---
+++ task.js
@@ -0,0 +1,7 @@
+let movies = [
+    {tittle: "Inception" , year:2010, rating: 8.8 , genre: "Sci-Fi" },
+    {tittle: "Titanic" ,year:1997, rating: 7.9 , genre: "Romance"},
+    {tittle: "Inception" , year:1999, rating: 8.8 , genre: "Sci-Fi" },
+    {tittle: "Inception" , year:2009, rating: 8.8 , genre: "Sci-Fi" },
+    {tittle: "Inception" , year:2004, rating: 8.8 , genre: "Sci-Fi" },
+]
